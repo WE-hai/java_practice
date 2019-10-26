65f58a5f5823ee9eba0420c1e35a52c27b027c87
WE-hai <943239470@qq.com>
2019-10-26 21:09:19 +0800

    add

---
+++ 10-26/Variable.java
@@ -0,0 +1,27 @@
+public class Variable{
+	public static void main(String[] args){
+		int a = 12;
+		System.out.println(a);
+		
+		int minInt = Integer.MIN_VALUE;
+		int maxInt = Integer.MAX_VALUE;
+		System.out.println(minInt);
+		System.out.println(maxInt);
+		System.out.println("==================");
+		short minShort = Short.MIN_VALUE;
+		short maxShort = Short.MAX_VALUE;
+		System.out.println(minShort);
+		System.out.println(maxShort);
+		System.out.println("==================");
+        byte minByte = Byte.MIN_VALUE;
+		byte maxByte = Byte.MAX_VALUE;
+		System.out.println(minByte);
+		System.out.println(maxByte);
+		System.out.println("==================");
+		long minLong = Long.MIN_VALUE;
+		long maxLong = Long.MAX_VALUE;
+		System.out.println(minLong);
+		System.out.println(maxLong);
+	}
+}
+
