7db9d85e515f7dbb68d2b4eca95d603a24ac538f
WE-hai <943239470@qq.com>
2019-10-26 21:49:09 +0800

    add

---
+++ 10-26/Char.java
@@ -0,0 +1,10 @@
+﻿public class Char{
+	public static void main(String[] args){
+		char a = 'A';
+		System.out.println(a);
+		char b = '家';
+		char c = '我';
+		char d = '爱';
+		System.out.print(cdcb);
+	}
+}
---
+++ 10-26/Char1.java
@@ -0,0 +1,14 @@
+public class Char1{
+	public static void main(String[] args){
+		char a = 'A';
+		System.out.println(a);
+		char b = '家';
+		char c = '我';
+		char d = '爱';
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(c);
+		System.out.println(b);
+		
+	}
+}
