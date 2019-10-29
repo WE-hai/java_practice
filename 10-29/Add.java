ce307a38de8e256b00b80cac262e7cf9a43b5c29
WE-hai <943239470@qq.com>
2019-10-29 21:50:34 +0800

    add

---
+++ 10-29/Add.java
@@ -0,0 +1,8 @@
+public class Add {
+	public static void main (String[] args) {
+	int x = Integer.parseInt(args[0]);
+	int y = Integer.parseInt(args[1]);
+	int result = x + y;
+	System.out.println(result);
+	}
+}
