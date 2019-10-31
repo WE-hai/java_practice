4a2b9d94da043f4433a16125d29b67607eda0884
WE-hai <943239470@qq.com>
2019-10-31 23:24:49 +0800

    add

---
+++ 10-31/Game.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Game {
+	public static void main (String[] args) {
+	Random random = new Random();
+	Scanner scanner = new Scanner(System.in);
+	int n = random.nextInt(200);
+	int times = 10;
+	for(int i = 1;i <= times;i++) {
+		System.out.print("请输入要猜的数字：");
+		int x = scanner.nextInt();
+		if(x==n) {
+			System.out.println("恭喜你，第"+i+"次机会猜测成功！");
+			return;
+		} else if(x < n) {
+			System.out.println("第"+i+"次机会，猜小了！");
+		} else {
+			System.out.println("第"+i+"次机会，猜大了！");
+		}
+	}
+	System.out.println("Game Over");
+	System.out.println("数字是："+n);
+	}
+}
