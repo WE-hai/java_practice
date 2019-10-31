7d87b3e855679c89d7b083a254792be282490510
WE-hai <943239470@qq.com>
2019-10-31 23:39:23 +0800

    add

--- 10-31/GuessNumber.java
+++ 10-31/GuessNumber.java
@@ -1,22 +1,22 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class Game {
+public class GuessNumber {
 	public static void main (String[] args) {
 	Random random = new Random();
 	Scanner scanner = new Scanner(System.in);
 	int n = random.nextInt(200);
 	int times = 10;
-	for(int i = 1;i <= times;i++) {
+	for(int i = 1;i <= times;i++) {	
 		System.out.print("请输入要猜的数字：");
 		int x = scanner.nextInt();
-		if(x==n) {
+		if(x == n) {
 			System.out.println("恭喜你，第"+i+"次机会猜测成功！");
 			return;
 		} else if(x < n) {
-			System.out.println("第"+i+"次机会，猜小了！");
+			System.out.println("第"+i+"次机会，猜小了");
 		} else {
-			System.out.println("第"+i+"次机会，猜大了！");
+			System.out.println("第"+i+"次机会，猜大了");
 		}
 	}
 	System.out.println("Game Over");
