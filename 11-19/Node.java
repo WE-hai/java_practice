e2d01d8703463f53eda84598627a0773c88c8094
WE-hai <943239470@qq.com>
2019-11-19 23:10:24 +0800

    链表

---
+++ 11-19/Node.java
@@ -0,0 +1,18 @@
+class Node {
+	int val;
+	Node next;
+	
+	Node() {
+		this.val = val;
+		this.next = next;
+	}
+	
+	Node(int val,Node next) {
+		this.val = val;
+		this.next = next;
+	}
+	Node(int val) {
+		this(val,null);
+	}
+}
+
