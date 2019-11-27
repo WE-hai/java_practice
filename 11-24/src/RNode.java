96a36a86d77ff2d9b98ec48991037cc9a8e701cc
WE-hai <943239470@qq.com>
2019-11-27 14:23:25 +0800

    add

---
+++ 11-24/src/RNode.java
@@ -0,0 +1,14 @@
+class RNode {
+    int val;
+    RNode next;
+    RNode random;
+
+    RNode() {
+        int val;
+        RNode next = null;
+        RNode random = null;
+    }
+    RNode(int val) {
+        this.val = val;
+    }
+}
