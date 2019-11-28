f3ab9080872d146798572251bbe4a621a52013ca
WE-hai <943239470@qq.com>
2019-11-28 21:41:18 +0800

    双向链表

---
+++ 11-24/src/doubly_list/MyLinkedList.java
@@ -0,0 +1,22 @@
+package doubly_list;
+
+class MyLinkedList {
+    private Node head;
+    private Node last;
+
+    public void pushFront(int val) {
+        Node node = new Node(val);
+    }
+    public void popFront() {
+
+    }
+    public void pushBack(int val) {
+
+    }
+    public void popBack() {
+
+    }
+    public void reset() {
+
+    }
+}
---
+++ 11-24/src/doubly_list/Node.java
@@ -0,0 +1,12 @@
+package doubly_list;
+
+class Node {
+    int size;
+    int val;
+    Node prev;
+    Node next;
+
+    Node(int val) {
+        this.val = val;
+    }
+}
