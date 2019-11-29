521f667255086afccbc2c104835014c6dcc310b6
WE-hai <943239470@qq.com>
2019-11-29 18:25:21 +0800

    单链表

---
+++ 11-24/src/single_list/MyLinkedList.java
@@ -0,0 +1,22 @@
+package single_list;
+
+public class MyLinkedList {
+    private Node head;
+    private Node last;
+
+    public void pushFront(int val) {
+
+    }
+    public void popFront() {
+
+    }
+    public void pushBack(int val) {
+
+    }
+    public void popBack() {
+
+    }
+    public void reset() {
+
+    }
+}
---
+++ 11-24/src/single_list/Node.java
@@ -0,0 +1,12 @@
+package single_list;
+
+class Node {
+    int val;
+    Node next;
+    Node last;
+
+    Node(int val) {
+        this.val = val;
+    }
+
+}
