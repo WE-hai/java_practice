3bdb865227684164904d66566ab7edb3fe6e874c
WE-hai <943239470@qq.com>
2019-12-19 23:42:36 +0800

    除

---
+++ 12-19/maths/Division.java
@@ -0,0 +1,60 @@
+package maths;
+
+import java.io.*;
+import maths.Operation;
+
+public class Division extends Operation {
+
+    static String ch = "/";
+
+    public Division(int n) {
+        super(ch,n);
+    }
+
+    @Override
+    public void operation() {
+        correctAnswer = op1 / op2;
+        remainder = op1 % op2;
+    }
+
+    public String isCorrect()
+    {
+        if(usersAnswer == correctAnswer && remainder == usersRemainder)
+            return "回答正确";
+        else
+            return "回答错误";
+    }
+
+    public String ptintQA()
+    {
+        operation();
+        return "答案："+op1+" "+ch+" "+op2+" = "+correctAnswer+" "+remainder;
+    }
+
+    @Override
+    public void isNumRight() {
+        while(op2 == 0)
+            getRanNum();
+    }
+
+    public void setRange(){
+        minRange = 0;
+        maxRange = maxInt;
+    }
+
+    public void writeToFile(File aFile)
+    {
+        try
+        {
+            PrintWriter out = new PrintWriter(new FileWriter(aFile,true));
+            out.println("题目："+op1+" "+ch+" "+op2);
+            out.println("你的答案："+usersAnswer+" "+usersRemainder + "    "+ "正确答案："+correctAnswer+" "+remainder);
+            out.close();
+        }catch(FileNotFoundException e){
+            System.err.println("File not found!" );
+        }catch(IOException e2){
+            e2.printStackTrace();
+        }       
+    }
+}
+
