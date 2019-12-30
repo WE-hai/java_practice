0222f568f68fe7474d87fb8c613bf8f41a752f07
WE-hai <943239470@qq.com>
2019-12-30 18:25:01 +0800

    String

---
+++ 12-30/src/Practice.java
@@ -0,0 +1,9 @@
+public class Practice {
+    //StringBuilder 不可变性
+    public static void main(String[] args) {
+        String s = "hello";
+        s += " ";
+        s += "world";
+        System.out.println(s);
+    }
+}
