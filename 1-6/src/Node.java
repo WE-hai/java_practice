2612a6d29896d41609275141ec6a8d57248f2493
WE-hai <943239470@qq.com>
2020-01-06 21:38:47 +0800

    二叉树

---
+++ 1-6/src/Node.java
@@ -0,0 +1,15 @@
+public class Node {
+    int value;
+    Node left;
+    Node right;
+
+    public Node() {
+        value = 0;
+        left = right = null;
+    }
+
+    public Node(int value) {
+        this.value = value;
+        left = right = null;
+    }
+}
