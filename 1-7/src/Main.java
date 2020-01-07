5e3f13d461f807c7eaa17c59ab5acb42d70a117e
WE-hai <943239470@qq.com>
2020-01-07 21:22:53 +0800

    栈

---
+++ 1-7/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        MyStack2 stack = new MyStack2();
+        stack.push(1);
+        System.out.println(stack.pop());
+        MyStack st = new MyStack();
+        st.push(2);
+        System.out.println(st.pop());
+    }
+}
