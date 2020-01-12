2f7222efd7cec46e50a55b606dd3359ed4dd3198
WE-hai <943239470@qq.com>
2020-01-12 19:08:33 +0800

    add

---
+++ 1-12/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        String s1="abc"+"def";//1
+        String s2=new String(s1);//2
+        if(s1.equals(s2))//3
+            System.out.println(".equals succeeded");//4
+        if(s1==s2)//5
+            System.out.println("==succeeded");//6
+    }
+}
