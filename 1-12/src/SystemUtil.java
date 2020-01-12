ba77c6318c4bfbde54a7c4b4f69b8fd399545a29
WE-hai <943239470@qq.com>
2020-01-12 19:09:08 +0800

    add

---
+++ 1-12/src/SystemUtil.java
@@ -0,0 +1,8 @@
+public class SystemUtil{
+    public static boolean isAdmin(String userId){
+        return userId.toLowerCase()=="admin";
+    }
+    public static void main(String[] args){
+        System.out.println(isAdmin("Admin"));
+    }
+}
