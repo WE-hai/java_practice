6feb3f177bdbbcddb886f6e1033f15da7f45d0ce
WE-hai <943239470@qq.com>
2020-01-16 17:58:37 +0800

    二维数组

---
+++ 1-16/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    //foreach循环
+    public static void main(String[] args) {
+        String[] books = {"语文","数学","英语"};
+        for (String book : books) {
+            System.out.println(book);
+        }
+    }
+}
