11236adfe4e0ff4eb075b8957dec32822435e426
WE-hai <943239470@qq.com>
2020-02-04 17:52:40 +0800

    作业

---
+++ 2-4/src/While.java
@@ -0,0 +1,14 @@
+public class While {
+    public void loop() {
+        int x= 10;
+        while ( x>10 ) {
+            System.out.print("x minus one is " + (x - 1));
+            x -= 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        boolean ret = false;
+
+    }
+}
