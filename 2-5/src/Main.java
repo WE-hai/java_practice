66b8e6261e1c2de2d0102389249d9e92caaf62fb
WE-hai <943239470@qq.com>
2020-02-05 18:31:26 +0800

    代码块

---
+++ 2-5/src/Main.java
@@ -0,0 +1,12 @@
+//代码块
+public class Main {
+    public static void main(String[] args) {
+        {
+            int a;
+            //System.out.println(a);
+            a = 7;
+            System.out.println(a);
+        }
+        //System.out.println(a);
+    }
+}
