801df5f02f8bc1f88c6c9ef64a77addb222647e8
WE-hai <943239470@qq.com>
2020-02-10 16:56:22 +0800

    变量使用规则

---
+++ 2-10/src/ScopeTest2.java
@@ -0,0 +1,8 @@
+public class ScopeTest2 {
+    public static void main(String[] args) {
+        int i;
+        for (i = 0;i < 10;i++) {
+            System.out.println("hello!");
+        }
+    }
+}
