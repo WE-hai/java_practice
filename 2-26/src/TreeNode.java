08b4c1fff5d4a460b76e02a2f75a6c56a7f14939
WE-hai <943239470@qq.com>
2020-02-26 15:35:50 +0800

    树结点

---
+++ 2-26/src/TreeNode.java
@@ -0,0 +1,15 @@
+public class TreeNode {
+    int value;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode() {
+        value = 0;
+        left = right = null;
+    }
+
+    public TreeNode(int value) {
+        this.value = value;
+        left = right = null;
+    }
+}
