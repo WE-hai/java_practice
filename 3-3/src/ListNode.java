14cd06e5961450b5193ea0ea89561bbe5f150817
WE-hai <943239470@qq.com>
2020-03-03 17:04:07 +0800

    链表节点

---
+++ 3-3/src/ListNode.java
@@ -0,0 +1,17 @@
+public class ListNode {
+    int value;
+    ListNode next;
+    ListNode prev;
+
+    public ListNode() {
+        value = 0;
+        next = null;
+        prev = null;
+    }
+
+    public ListNode(int value) {
+        this.value = value;
+        next = null;
+        prev = null;
+    }
+}
