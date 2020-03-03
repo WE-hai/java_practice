06a165239bc7276c712b66ce020b3e1f413af20a
WE-hai <943239470@qq.com>
2020-03-03 17:04:55 +0800

    双向链表

---
+++ 3-3/src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+        MylinkedList list = new MylinkedList();
+        list.pushFront(1);
+        list.pushFront(2);
+        list.pushFront(3);
+        list.pushFront(4);
+        System.out.println(list);
+        list.reset();
+        list.pushBack(1);
+        list.pushBack(2);
+        list.pushBack(3);
+        list.pushBack(4);
+        System.out.println(list);
+        list.popFront();
+        list.popFront();
+        list.popFront();
+        System.out.println(list);
+        list.popFront();
+        System.out.println(list);
+        list.pushBack(1);
+        list.pushBack(2);
+        list.pushBack(3);
+        list.pushBack(4);
+        System.out.println(list);
+        list.popBack();
+        list.popBack();
+        list.popBack();
+        System.out.println(list);
+        list.popBack();
+        System.out.println(list);
+    }
+}
