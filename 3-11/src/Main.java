708b2ce22ded75bed95fd23ef626291859628c0d
WE-hai <943239470@qq.com>
2020-03-11 16:34:41 +0800

    作业

---
+++ 3-11/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int i=0;
+        Integer j = new Integer(0);
+        System.out.println(i==j);
+        System.out.println(j.equals(i));
+    }
+}
