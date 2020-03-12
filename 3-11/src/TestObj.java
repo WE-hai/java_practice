a0cd929ae1d2242deac68be533b0aec70261068e
WE-hai <943239470@qq.com>
2020-03-12 17:00:57 +0800

    add

---
+++ 3-11/src/TestObj.java
@@ -0,0 +1,10 @@
+public class TestObj{
+    public static void main(String[] args){
+        Object o = new Object(){
+            public boolean equals(Object obj){
+                return true;
+            }
+        };
+        System.out.println(o.equals("Fred"));
+    }
+}
