1930b447f7d73fbbb0e037b728dc079975eb8ad1
WE-hai <943239470@qq.com>
2020-03-23 16:48:37 +0800

    add

---
+++ 3-23 作业/src/Node.java
@@ -0,0 +1,11 @@
+public class Node {
+    int key;
+    int value;
+    Node next = null;
+    public Node(int key, int value) {
+        this.key  = key;
+        this.value = value;
+        next = null;
+    }
+}
+
