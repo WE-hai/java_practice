2bc7e01a1e68c617bc9cef121a085df220f3574b
WE-hai <943239470@qq.com>
2020-04-02 16:02:28 +0800

    add

---
+++ 4-2 每日一题/src/TestThread.java
@@ -0,0 +1,31 @@
+class Test {
+    private int data;
+    int result = 0;
+    public void m() {
+        result += 2;
+        data += 2;
+        System.out.print(result + " " + data);
+    }
+}
+class ThreadExample extends Thread {
+    private Test mv;
+    public ThreadExample(Test mv) {
+        this.mv = mv;
+    }
+    public void run() {
+        synchronized(mv) {
+            mv.m();
+        }
+    }
+}
+public class TestThread {
+    public static void main(String args[]) {
+        Test mv = new Test();
+        Thread t1 = new ThreadExample(mv);
+        Thread t2 = new ThreadExample(mv);
+        Thread t3 = new ThreadExample(mv);
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
