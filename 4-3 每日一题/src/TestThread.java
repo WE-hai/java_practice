ccd98c4ef7c804ca2c05426de75c9373b32f980c
WE-hai <943239470@qq.com>
2020-04-03 17:31:26 +0800

    add

---
+++ 4-3 每日一题/src/TestThread.java
@@ -0,0 +1,18 @@
+public class TestThread {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                System.out.print("2");
+                }
+        });
+        t.start();
+        t.join();
+        System.out.print("1");
+    }
+}
