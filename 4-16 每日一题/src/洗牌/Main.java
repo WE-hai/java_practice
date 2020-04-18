9bace9c83afdf9252ac986eb78b0c035ad2627db
WE-hai <943239470@qq.com>
2020-04-18 14:24:39 +0800

    洗牌

---
+++ 4-16 每日一题/src/洗牌/Main.java
@@ -0,0 +1,15 @@
+package 洗牌;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            int T = scanner.nextInt();
+            int n = scanner.nextInt();
+            int k = scanner.nextInt();
+            int[] arr = new int[2*n];
+        }
+    }
+}
