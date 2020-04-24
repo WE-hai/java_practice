1cb72c63b15b38f4714e32dbddebcde7a6a5a2c9
WE-hai <943239470@qq.com>
2020-04-24 15:10:13 +0800

    add

---
+++ 4-21 每日一题/src/链表分割/ListNode.java
@@ -0,0 +1,15 @@
+package 链表分割;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode(int val,ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+}
