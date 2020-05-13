f1413e9559cb35e8dd12f813986bdaf418717380
WE-hai <943239470@qq.com>
2020-05-13 14:00:05 +0800

    简单错误记录

---
+++ 5-11 每日一题/src/简单错误记录/Main.java
@@ -0,0 +1,12 @@
+package 简单错误记录;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+
+        }
+    }
+}
