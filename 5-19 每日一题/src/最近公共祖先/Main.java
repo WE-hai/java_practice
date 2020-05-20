2eed22114f2283149002e23478b84ac1c0ec90f6
WE-hai <943239470@qq.com>
2020-05-20 16:16:25 +0800

    最近公共祖先

---
+++ 5-19 每日一题/src/最近公共祖先/Main.java
@@ -0,0 +1,14 @@
+package 最近公共祖先;
+
+public class Main {
+    public int getLCA(int a,int b) {
+        while (a != b) {
+            if (a > b) {
+                a /= 2;
+            } else {
+                b /= 2;
+            }
+        }
+        return a;
+    }
+}
