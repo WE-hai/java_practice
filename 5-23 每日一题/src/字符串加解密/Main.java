2b8f189c2f3c655143f7cd019b736068134a99e4
WE-hai <943239470@qq.com>
2020-05-31 14:08:18 +0800

    字符串加解密

---
+++ 5-23 每日一题/src/字符串加解密/Main.java
@@ -0,0 +1,59 @@
+package 字符串加解密;
+
+import java.util.Scanner;
+
+public class Main {
+    public static String jiaMi(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0;i < s.length();i++) {
+            char c = s.charAt(i);
+            if (c >= 'a' && c <= 'y') {
+                sb.append((char)(c-31));
+            } else if (c >= 'A' && c <= 'Y') {
+                sb.append((char)(c+33));
+            } else if (c == 'z') {
+                sb.append('A');
+            } else if (c == 'Z') {
+                sb.append('a');
+            } else if (c >= '0' && c <= '8') {
+                sb.append((char)(c+1));
+            } else if (c == '9') {
+                sb.append('0');
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+    public static String jieMi(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0;i < s.length();i++) {
+            char c = s.charAt(i);
+            if (c >= 'b' && c <= 'z') {
+                sb.append((char)(c-33));
+            } else if (c >= 'B' && c <= 'Z') {
+                sb.append((char)(c+31));
+            } else if (c == 'a') {
+                sb.append('Z');
+            } else if (c == 'A') {
+                sb.append('z');
+            } else if (c >= '1' && c <= '9') {
+                sb.append((char)(c-1));
+            } else if (c == '0') {
+                sb.append('9');
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            String s1 = scanner.nextLine();
+            String s2 = scanner.nextLine();
+            System.out.println(jiaMi(s1));
+            System.out.println(jieMi(s2));
+        }
+    }
+}
