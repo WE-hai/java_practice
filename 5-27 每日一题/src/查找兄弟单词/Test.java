708b12261f14f79bf403b01f682678ca38a6755c
WE-hai <943239470@qq.com>
2020-06-03 21:49:24 +0800

    add

---
+++ 5-27 每日一题/src/查找兄弟单词/Test.java
@@ -0,0 +1,43 @@
+package 查找兄弟单词;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            int num = in.nextInt();
+            String[] s = new String[num];
+            int count = 0;
+            for(int i = 0;i<num;i++){
+                s[i] = in.next();
+            }
+            String key = in.next();
+            char[] keyChar = key.toCharArray();
+            Arrays.sort(keyChar);
+            int no = in.nextInt();//第几个
+            ArrayList<String> list = new ArrayList<String>();
+            for(int i = 0;i<num;i++){
+                int c = check(key,s[i],keyChar);
+                count += c;
+                if(c==1)
+                    list.add(s[i]);
+            }
+            System.out.println(count);
+            Collections.sort(list);
+            if(count>=no)
+                System.out.println(list.get(no-1));
+        }
+    }
+    private static int check(String key,String word,char[] keyChar){
+        if(key.equals(word)||key.length()!=word.length())
+            return 0;
+        char[] wordChar = word.toCharArray();
+        Arrays.sort(wordChar);
+        return Arrays.equals(keyChar, wordChar)?1:0;
+    }
+}
