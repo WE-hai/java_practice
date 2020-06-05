22151008773f4e2b793b0c126ba1513b7ea42bb6
WE-hai <943239470@qq.com>
2020-06-05 13:58:54 +0800

    马戏团

---
+++ 5-29 每日一题/src/马戏团/Main.java
@@ -0,0 +1,12 @@
+package 马戏团;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            int n = scanner.nextInt();
+        }
+    }
+}
