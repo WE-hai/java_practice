46e924dddfe20ed71c70bc6acc291431dcbb4c1e
WE-hai <943239470@qq.com>
2020-06-24 19:02:05 +0800

    JDBC

---
+++ untitled/src/com/bit/DB.java
@@ -0,0 +1,28 @@
+package com.bit;
+
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DB {
+    private static DataSource dataSource;
+
+    static {
+        MysqlDataSource mysqlDataSource = new MysqlDataSource();
+        mysqlDataSource.setServerName("127.0.0.1");
+        mysqlDataSource.setPort(3306);
+        mysqlDataSource.setUser("root");
+        mysqlDataSource.setPassword("");
+        mysqlDataSource.setDatabaseName("boke");
+        mysqlDataSource.setUseSSL(false);
+        mysqlDataSource.setCharacterEncoding("utf8");
+
+        dataSource = mysqlDataSource;
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+}
