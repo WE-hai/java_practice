fc4cce0b7d649c5098f17067f3247fed7f13d016
WE-hai <943239470@qq.com>
2020-06-29 15:15:07 +0800

    ATM

---
+++ ATM/src/Account.java
@@ -0,0 +1,25 @@
+public class Account {
+    private String username;
+    private String password;
+    private double money;
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public double getMoney() {
+        return money;
+    }
+    public void setMoney(double money) {
+        this.money = money;
+    }
+
+}
+
