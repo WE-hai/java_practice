d1439adb0b5a77529bac78c830c018d8985dcf84
WE-hai <943239470@qq.com>
2020-07-08 21:07:20 +0800

    减

---
+++ 四则运算/src/maths/Subtraction.java
@@ -0,0 +1,30 @@
+package maths;
+
+public class Subtraction extends Operation{
+
+    static String ch = "-";
+
+    public Subtraction(int n) {
+        super(ch,n);
+    }
+
+    public void operation() {
+        correctAnswer = op1 - op2;
+    }
+
+    public void isNumRight(){
+        while(op1 == op2)
+            getRanNum();
+        if(op1 < op2)
+        {
+            int temp = op1;
+            op1 = op2;
+            op2 = temp;
+        }
+    }
+
+    public void setRange(){
+        minRange = -maxInt;
+        maxRange = maxInt;
+    }
+}
