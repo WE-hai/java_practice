986648d89fd5e03a199c033a3775c1e957f7021b
WE-hai <943239470@qq.com>
2020-07-10 12:27:01 +0800

    add

---
+++ math/src/maths/Operation.java
@@ -0,0 +1,64 @@
+package maths;
+
+import java.io.*;
+
+public abstract class Operation {
+    protected int op1,op2,remainder,usersRemainder,n,correctAnswer,usersAnswer,maxInt=1;
+    protected String ch;
+    protected long minRange,maxRange;
+
+    public Operation(String ch,int n) {
+        super();
+        this.ch = ch;
+        this.n = n;
+    }
+
+    public abstract void operation();
+    public abstract void isNumRight();
+    public abstract void setRange();
+
+    protected void getRanNum() {
+        op1 = (int)(Math.random()*Math.pow(10,n));
+        op2 = (int)(Math.random()*Math.pow(10,n));
+    }
+
+    public void setUsersAnswer(int usersAnswer,int usersRemainder)  {
+        this.usersAnswer = usersAnswer;
+        this.usersRemainder = usersRemainder;
+    }
+
+    public void setUsersAnswer(int usersAnswer) {
+        setUsersAnswer(usersAnswer,0);
+    }
+
+    public String isCorrect() {
+        if(usersAnswer == correctAnswer)
+            return "回答正确";
+        else
+            return "回答错误";
+    }
+
+    public String printQuestion() {
+        getRanNum();
+        isNumRight();
+        return op1+" "+ch+" "+op2+" = ";
+    }
+
+    public String ptintQA() {
+        operation();
+        return "答案："+op1+" "+ch+" "+op2+" = "+correctAnswer;
+    }
+
+    public void writeToFile(File aFile) {
+        try  {
+            PrintWriter out = new PrintWriter(new FileWriter(aFile,true));
+            out.println("题目："+op1+" "+ch+" "+op2);
+            out.println("你的答案："+usersAnswer + "    "+ "正确答案："+correctAnswer);
+            out.close();
+        }catch(FileNotFoundException e){
+            System.err.println("File not found!" );
+        }catch(IOException e2){
+            e2.printStackTrace();
+        }
+    }
+}
