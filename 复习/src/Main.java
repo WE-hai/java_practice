9ff3cdf0f9b2e23899f4015df8d93f76cd10bf54
WE-hai <943239470@qq.com>
2020-07-13 20:41:29 +0800

    add

---
+++ 复习/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int maxValue = Integer.MAX_VALUE;
+        int minValue = Integer.MIN_VALUE;
+        System.out.println(maxValue+1);
+        System.out.println(minValue-1);
+        System.out.println(maxValue);
+        System.out.println(minValue);
+
+        System.out.println("==============================");
+        System.out.println(Long.MAX_VALUE);
+        System.out.println(Long.MIN_VALUE);
+
+        System.out.println("==============================");
+
+    }
+}
