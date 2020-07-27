2b064b22c69db0cbff31b68c9ee21b5bc52ee00d
WE-hai <943239470@qq.com>
2020-07-27 11:10:31 +0800

    add

---
+++ 7-22/src/TestThread.java
@@ -0,0 +1,59 @@
+class MyThread2 implements Runnable {
+    private boolean flag;
+    private Object obj;
+    public MyThread2(boolean flag, Object obj) {
+        super();
+        this.flag = flag;
+        this.obj = obj;
+    }
+    public void waitMethod() {
+        synchronized (obj) {
+            try {
+                while (true) {
+                    System.out.println("wait()方法开始.. " + Thread.currentThread().getName());
+                    obj.wait();
+                    System.out.println("wait()方法结束.. " + Thread.currentThread().getName());
+                    return;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void notifyMethod() {
+        synchronized (obj) {
+            try {
+                System.out.println("notify()方法开始.. " + Thread.currentThread().getName());
+                obj.notify();
+                System.out.println("notify()方法结束.. " + Thread.currentThread().getName());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void run() {
+        if (flag) {
+            this.waitMethod();
+        } else {
+            this.notifyMethod();
+        }
+    }
+}
+
+
+public class TestThread {
+    public static void main(String[] args) throws InterruptedException {
+        Object object = new Object();
+        MyThread2 waitThread = new MyThread2(true, object);
+        MyThread2 notifyThread = new MyThread2(false, object);
+        Thread thread1 = new Thread(waitThread, "wait线程");
+        Thread thread2 = new Thread(notifyThread, "notify线程");
+        thread1.start();
+        Thread.sleep(1000);
+        thread2.start();
+        System.out.println("main方法结束!!");
+    }
+}
