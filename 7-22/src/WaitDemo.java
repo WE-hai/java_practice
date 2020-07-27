d4e52d5d3c2a0e350319c4f0275b9d0848179f52
WE-hai <943239470@qq.com>
2020-07-27 11:18:17 +0800

    add

---
+++ 7-22/src/WaitDemo.java
@@ -0,0 +1,10 @@
+public class WaitDemo {
+    public static void main(String[] args) throws InterruptedException {
+        Object object = new Object();
+        synchronized (object) {
+            System.out.println("等待中...");
+            object.wait();
+            System.out.println("等待已过...");
+        }
+        System.out.println("main方法结束..."); }
+}
