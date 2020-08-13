0006d481fa155f88e26d097ea8579ed8febd5b21
WE-hai <943239470@qq.com>
2020-08-13 13:48:48 +0800

    Test

---
+++ 8-12/src/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        double d1 = -0.5;
+        System.out.println("Ceil d1 = "+Math.ceil(d1));
+        System.out.println("floor d1 = "+Math.floor(d1));
+
+    }
+}
