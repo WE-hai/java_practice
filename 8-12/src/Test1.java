56f73f041f00f2cfaf40152cf0206a3fd5175242
WE-hai <943239470@qq.com>
2020-08-13 13:49:49 +0800

    Test

---
+++ 8-12/src/Test1.java
@@ -0,0 +1,10 @@
+public class Test1 {
+    public static int a = 1;
+    public static void main(String[] args) {
+        int a = 10;
+        a++;
+        Test1.a++;
+        Test1 t = new Test1();
+        System.out.println("a = " + a + " t.a = " + t.a);
+    }
+}
