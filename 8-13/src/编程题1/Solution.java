c559b38865b0ebbbb730e1d79bc934194cc324fa
WE-hai <943239470@qq.com>
2020-08-14 17:55:31 +0800

    蜗牛 动态规划

---
+++ 8-13/src/编程题1/Solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+	public int daycost (int len, int m, int n) {
+		int[] dp = new int[len];
+		for (int i = 0;i < len;i++) {
+			dp[i] = dp[i-1]+m;
+			if (dp[i] > len) {
+				dp[i] -= n;
+			} else {
+				return i;
+			}
+		}
+		return -1;
+	}
+	public static void main(String[] args) {
+		Main t = new Main();
+		System.out.println(t.daycost(10,4,3));
+	}
+		
+}
