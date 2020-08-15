4ce644e1343b7b267a1ce9da5a9d7e61d98c3cb5
WE-hai <943239470@qq.com>
2020-08-15 13:41:40 +0800

    add

---
+++ 8-11/src/Test2.java
@@ -0,0 +1,9 @@
+public class Test2 {
+    public static void main(String[] args) {
+        char c = 127;
+        int sum = 200;
+        c += 1;
+        sum += c;
+        System.out.println(sum);
+    }
+}
