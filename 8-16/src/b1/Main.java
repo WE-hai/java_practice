507ed4fdc4a67190400cd25c81d8e507defeddd9
WE-hai <943239470@qq.com>
2020-08-17 14:04:32 +0800

    add

---
+++ 8-16/src/b1/Main.java
@@ -0,0 +1,14 @@
+package b1;
+
+public class Main {
+    public int CalulateMethodCount (int num_money) {
+        // write code here
+        int count = 0;
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
