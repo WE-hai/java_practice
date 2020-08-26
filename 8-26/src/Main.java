5044433662a8247653e345afc953fa846325f6c4
WE-hai <943239470@qq.com>
2020-08-26 15:02:44 +0800

    add

---
+++ 8-26/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public String str = "6";
+    public static void main(String[] args) {
+        SendValue sv = new SendValue();
+        sv.change(sv.str);
+        System.out.println(sv.str);
+    }
+    public void change(String str) {
+        str = "10";
+    }
+}
