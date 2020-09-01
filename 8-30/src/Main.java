d8b402fbf05885d7c959f8d60f1f1539542ecd51
WE-hai <943239470@qq.com>
2020-09-01 16:04:08 +0800

    add

---
+++ 8-30/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    //变态青蛙跳台阶
+    public int JumpFloorII(int target) {
+        int ret = 1;
+        for(int i = 2;i <= target;i++) {
+            ret *= 2;
+        }
+        return ret;
+    }
+
+    public int JumpFloorII2(int target) {
+        return 1 << (target-1);
+    }
+}
