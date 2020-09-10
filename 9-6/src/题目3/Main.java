31ab6404810c78fbf76f8db53fd60bb3fac3552f
WE-hai <943239470@qq.com>
2020-09-10 16:30:02 +0800

    add

---
+++ 9-6/src/题目3/Main.java
@@ -0,0 +1,24 @@
+package 题目3;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            int n = scanner.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0;i < n;i++) {
+                arr[i] = scanner.nextInt();
+            }
+            for (int i = 0;i < n;i++) {
+                if (arr[i] == i) {
+
+                }
+            }
+            System.out.println("TES");
+
+            System.out.println("NO");
+        }
+    }
+}
