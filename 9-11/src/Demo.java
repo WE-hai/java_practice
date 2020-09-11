c90761a2ce5c8f0ee7b454a9ae01ce56da6fdbe7
WE-hai <943239470@qq.com>
2020-09-11 18:05:54 +0800

    add

---
+++ 9-11/src/Demo.java
@@ -0,0 +1,22 @@
+public class Demo{
+    float func0() {
+        byte i = 1;
+        return i;
+    }
+    float func1() {
+        int i = 1;
+        return i;
+    }
+    float func2() {
+        short i = 2;
+        return i;
+    }
+    float func3() {
+        long i = 3;
+        return i;
+    }
+    float func4() {
+        double i = 4;
+        return (float) i;
+    }
+}
