f81e0f013a6627a0f5d964c62c1867263d13c515
WE-hai <943239470@qq.com>
2020-09-17 16:09:17 +0800

    add

---
+++ 9-15 笔试/src/笔试题目2/Test.java
@@ -0,0 +1,17 @@
+package 笔试题目2;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            char[][] strings = new char[200][200];
+            for (int i = 0;i < strings.length;i++) {
+                for (int j = 0;j < strings[i].length;j++) {
+
+                }
+            }
+        }
+    }
+}
