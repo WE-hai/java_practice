8f47b553ce4158eca9b1703ced81c0f2cc6ce76d
WE-hai <943239470@qq.com>
2020-09-17 16:09:44 +0800

    add

---
+++ 9-15 笔试/src/笔试题目3/Main.java
@@ -0,0 +1,12 @@
+package 笔试题目3;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+
+        }
+    }
+}
