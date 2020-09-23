c4428dab7e5a7c18346501b1a08bbc93d0b36d7b
WE-hai <943239470@qq.com>
2020-09-23 20:42:43 +0800

    add

---
+++ 9-23/src/去哪题目2/Main.java
@@ -0,0 +1,12 @@
+package 去哪题目2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+
+        }
+    }
+}
