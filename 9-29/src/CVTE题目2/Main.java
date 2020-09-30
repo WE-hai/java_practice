c050da668018c5e8bdf14ba0cebbae0ad9151bb1
WE-hai <943239470@qq.com>
2020-09-30 15:55:53 +0800

    add

---
+++ 9-29/src/CVTE题目2/Main.java
@@ -0,0 +1,12 @@
+package CVTE题目2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+
+        }
+    }
+}
