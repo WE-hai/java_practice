7e18d531e942f14374bba6740d146dc5bfbd02a8
WE-hai <943239470@qq.com>
2020-10-09 19:48:03 +0800

    add

---
+++ 10-9/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 78;
+        System.out.println(!((a == 45) || (b <= 45)));
+        System.out.println(((a != 45) && (b > 45)));
+    }
+}
