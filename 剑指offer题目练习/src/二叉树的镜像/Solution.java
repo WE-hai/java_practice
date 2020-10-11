22ec8327bd5e97e85877081ed0fc935c3ffffa78
WE-hai <943239470@qq.com>
2020-10-11 18:20:06 +0800

    add

---
+++ 剑指offer题目练习/src/二叉树的镜像/Solution.java
@@ -0,0 +1,14 @@
+package 二叉树的镜像;
+
+public class Solution {
+    public void Mirror(TreeNode root) {
+        if(root == null){
+            return;
+        }
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        Mirror(root.left);
+        Mirror(root.right);
+    }
+}
