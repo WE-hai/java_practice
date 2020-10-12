70497a1bf4c8a251c5db0cef7e49b22e7555f381
WE-hai <943239470@qq.com>
2020-10-12 13:03:36 +0800

    add

---
+++ 剑指offer题目练习/src/反转链表/Main.java
@@ -0,0 +1,14 @@
+package 反转链表;
+
+public class Main {
+    public ListNode ReverseList(ListNode head) {
+        ListNode node = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = node;
+            node = head;
+            head = next;
+        }
+        return node;
+    }
+}
