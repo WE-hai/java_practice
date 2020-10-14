050ad49ebd73b9a4ceb995d51d8448d03296df1f
WE-hai <943239470@qq.com>
2020-10-14 20:34:28 +0800

    add

---
+++ 剑指offer题目练习/src/合并两个排序的链表/Solution.java
@@ -0,0 +1,22 @@
+package 合并两个排序的链表;
+
+public class Solution {
+    public ListNode Merge(ListNode list1,ListNode list2) {
+        if (list1 == null && list2 == null) {
+            return null;
+        }
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+        if (list1.val <= list2.val) {
+            list1.next = Merge(list1.next,list2);
+            return list1;
+        } else {
+            list2.next = Merge(list1,list2.next);
+            return list2;
+        }
+    }
+}
