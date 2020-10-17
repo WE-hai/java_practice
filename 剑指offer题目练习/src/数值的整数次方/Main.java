4e17bbad0c674c8114be7ad68da944c09e512c14
WE-hai <943239470@qq.com>
2020-10-17 21:16:43 +0800

    add

---
+++ 剑指offer题目练习/src/数值的整数次方/Main.java
@@ -0,0 +1,28 @@
+package 数值的整数次方;
+
+public class Main {
+    // 递归写法
+    public double Power(double base, int exp) {
+        boolean flag = exp < 0;
+        if (flag) {
+            exp = -exp;
+        }
+        double result = getPower(base, exp);
+        return flag ? 1 / result : result;
+    }
+
+    public double getPower(double base, int exp) {
+        if (exp == 0) {
+            return 1;
+        }
+        if (exp == 1) {
+            return base;
+        }
+        double ans = getPower(base, exp >> 1);
+        ans *= ans;
+        if ((exp & 1) == 1) {
+            ans *= base;
+        }
+        return ans;
+    }
+}
