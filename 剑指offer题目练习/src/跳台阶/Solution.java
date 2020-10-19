4997ab96c1ac222a6f64da00f2a14d8c222a575f
WE-hai <943239470@qq.com>
2020-10-19 18:41:40 +0800

    add

---
+++ 剑指offer题目练习/src/跳台阶/Solution.java
@@ -0,0 +1,13 @@
+package 跳台阶;
+
+public class Solution {
+    public int JumpFloor(int target) {
+        if (target == 1) {
+            return 1;
+        }
+        if (target == 2) {
+            return 2;
+        }
+        return JumpFloor(target - 1) + JumpFloor(target - 2);
+    }
+}
