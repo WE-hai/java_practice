f4acd0211538aa0cdcbf0ac7091a196447b18776
WE-hai <943239470@qq.com>
2020-10-20 18:33:03 +0800

    add

---
+++ 剑指offer题目练习/src/用两个栈实现队列/Main.java
@@ -0,0 +1,21 @@
+package 用两个栈实现队列;
+
+import java.util.Stack;
+
+public class Main {
+    Stack<Integer> stack1 = new Stack<>();
+    Stack<Integer> stack2 = new Stack<>();
+
+    public void push(int node) {
+        stack1.push(node);
+    }
+
+    public int pop() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+}
