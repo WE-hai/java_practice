87a4db4e77fb567b6c0dbda1d65fefe53fda5cdd
WE-hai <943239470@qq.com>
2020-11-25 20:06:36 +0800

    add

---
+++ 11-23 每日一题/src/Main.java
@@ -0,0 +1,11 @@
+class Test {
+    public static void hello() {
+        System.out.println("hello");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Test test = null;
+        test.hello();
+    }
+}
