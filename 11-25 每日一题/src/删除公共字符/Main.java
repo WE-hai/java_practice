4680a48fbde89e93d5caa8d1d9183815b18583c2
WE-hai <943239470@qq.com>
2020-11-29 17:00:10 +0800

    add

---
+++ 11-25 每日一题/src/删除公共字符/Main.java
@@ -0,0 +1,18 @@
+package 删除公共字符;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)  {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String s1 = sc.nextLine();
+            String s2 = sc.nextLine();
+            String pattern = "[" + s2 + "]";
+            String result = s1.replaceAll(pattern, "");
+            System.out.println(result);
+        }
+    }
+}
+
+
